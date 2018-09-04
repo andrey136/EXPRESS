ef0f5e249d4dfa7d4ff20d9cb0ddbb3bd706ad8c
Andrey Mardash <andrey.mardash@gmail.com>
2018-09-04 17:21:30 +0300

    Now the code can enable errors

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    // eslint-disable-line consistent-return
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 let todoList = [
-    { title: 'First' },
-    { title: 'Second' },
-    { title: 'Third' },
-    { title: 'Forth' },
+    { title: 'First', completed: true },
+    { title: 'Second', completed: true },
+    { title: 'Third', completed: true },
+    { title: 'Forth', completed: true },
 ];
 
 app.get('/', (req, res)=> {
